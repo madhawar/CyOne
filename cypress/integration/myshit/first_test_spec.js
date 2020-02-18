7517499279b5590fe40e2944c27da410854ae057
Madhawa Ratnayake <mdmadhawaratnayake@gmail.com>
2020-02-18 22:49:32 +0530

    first commit

---
+++ cypress/integration/myshit/first_test_spec.js
@@ -0,0 +1,9 @@
+describe ('Test Suite 1', function()
+{
+    it('Test Case 1', function()
+    {
+        cy.visit('https://www.staysure.co.uk/')
+        cy.title().should('eq', 'Staysure™ Travel Insurance - It\'s Worth Doing Right')
+    })
+
+})
